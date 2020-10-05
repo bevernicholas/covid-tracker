761599c0ee723672aff6d3453e11bd5b8f024925
Nick Bever <nicholasbever@Nicholass-MacBook-Air-2.local>
2020-10-05 10:17:50 -0700

    created skeleton and folder directory

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the COVID Tracker</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
